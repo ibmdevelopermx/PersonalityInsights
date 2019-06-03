763774ee51de367025a8908eb65d7708b99d0492
Isaac Carrada <isaac.yael.carrada@ibm.com>
2019-06-03 11:43:12 -0500

    First commit

---
+++ node_modules/watson-developer-cloud/natural-language-understanding/v1.d.ts
@@ -0,0 +1,7 @@
+import GeneratedNaturalLanguageUnderstandingV1 = require('./v1-generated');
+declare class NaturalLanguageUnderstandingV1 extends GeneratedNaturalLanguageUnderstandingV1 {
+    static VERSION_DATE_2016_01_23: string;
+    static VERSION_DATE_2017_02_27: string;
+    constructor(options: any);
+}
+export = NaturalLanguageUnderstandingV1;
