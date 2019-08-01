3985985249a17a8b2a6604a568b7b8aa56ef2666
Isaac Carrada <isaac.yael.carrada@ibm.com>
2019-08-01 14:40:10 -0500

    agregadas Credenciales

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var Estructurador = require('./Estructurador');
 //set IBM cloud credentials in this section
 var personalityInsights = new PersonalityInsightsV3({
   version_date: '2017-10-13',
-  username: '',
-  password: ''
+  username: 'apikey',
+  password: 'sespzkgaTh0V6Sp4ht5PD5f63b3AG7BF_aFwJXo3Q_9s'
 });
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
